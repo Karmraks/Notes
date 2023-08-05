185193a8c59c88f978b1561ccc9a21b4592b9aee
Oleh Starovoitenko <oleg04011999@gmail.com>
2023-08-05 19:28:35 +0200

    Initial commit

---
+++ Notes.Backend/Notes.Application/Notes/Commands/DeleteNote/DeleteCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Notes.Application.Notes.Commands.DeleteCommand;
+
+namespace Notes.Application.Notes.Commands.DeleteNote
+{
+    public class DeleteCommandValidator : AbstractValidator<DeleteNoteCommand>
+    {
+        public DeleteCommandValidator()
+        {
+            RuleFor(deleteNoteCommand =>
+                deleteNoteCommand.UserId).NotEqual(Guid.Empty);
+            RuleFor(deleteNoteCommand =>
+                deleteNoteCommand.Id).NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ Notes.Backend/Notes.WebApi/Models/UpdateNoteDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Notes.Commands.UpdateNote;
+
+namespace Notes.WebApi.Models
+{
+    public class UpdateNoteDto : IMapWith<UpdateNoteCommand>
+    {
+        public Guid Id { get; set; }
+        public string Tiltle { get; set; }
+        public string Details { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateNoteDto, UpdateNoteCommand>()
+                .ForMember(noteCommand => noteCommand.Id,
+                    opt => opt.MapFrom(noteDto => noteDto.Id))
+                .ForMember(noteCommand => noteCommand.Title,
+                    opt=> opt.MapFrom(noteDto => noteDto.Tiltle))
+                .ForMember(noteCommand => noteCommand.Details,
+                    opt=> opt.MapFrom(noteDto => noteDto.Details));
+        }
+    }   
+}
